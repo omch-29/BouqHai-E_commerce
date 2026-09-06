1120ff6598356102182632ea839e11949b12b443
omch-29 <chandwaniom29@gmail.com>
2026-09-06 18:42:07 +0530

    Added textarea for customization input

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -30,8 +30,7 @@ const Cart = () => {
                 // <div className="cart-item card" key={item.productId}>
                 //   <img src={getImageUrl(item.image)} alt={item.name} />
                 //   <div className=
-               
-                //   <div
+              
             
         
                 // </div>
